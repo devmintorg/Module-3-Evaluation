524252c7e1e6491d7326ba459d1bbe3d2a3c8a83
Schnablette <anntschnabel@gmail.com>
2022-08-28 13:54:35 -0400

    auction starting template

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,2 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+abstract contract Auction {
+
+    //Some, but not all of your state variables
+    uint256 timeOfAuction;
+
+    uint256 auctionEndTime;
+    uint256 auctionFinalized;
+
+    address topBidder;
+    address topBidAmount;
+
+    //Events
+    event auctionStarted();
+    event madeBid();
+    event auctionFinal();
+
+    constructor() { }
+
+    function startAuction() external { }
+
+    function bid() external payable { }
+
+    function finalizeAuction() external { }
+
+}
