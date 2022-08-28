24293b0b2a52772b36dcaccdf4f8199b1b8ae35b
Schnablette <anntschnabel@gmail.com>
2022-08-28 14:46:34 -0400

    test skeleton written

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("hardhat-docgen");
+require('solidity-coverage')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -10,4 +11,10 @@ module.exports = {
     clear: true,
     runOnCompile: true,
   },
+  // networks: {
+  //   goerli: {
+  //     url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+  //     accounts: [process.env.GOERLI_PRIVATE_KEY],
+  //   },
+  // },
 };
